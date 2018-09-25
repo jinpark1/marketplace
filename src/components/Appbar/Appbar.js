d510eb9f7a05d55d65398290b7cb84c95e770fb9
Jin Park <jinpark1dev@gmail.com>
2018-09-25 13:06:17 -0700

    added Link

--- src/components/Appbar/Appbar.js
+++ src/components/Appbar/Appbar.js
@@ -9,6 +9,7 @@ import MenuIcon from '@material-ui/icons/Menu';
 import ShoppingCart from '@material-ui/icons/ShoppingCart';
 import Badge from '@material-ui/core/Badge';
 import Drawer from '../Drawer/Drawer';
+import { Link } from 'react-router-dom';
 
 const styles = {
   root: {
@@ -23,6 +24,10 @@ const styles = {
   },
   appBar: {
     backgroundColor: 'grey',
+  },
+  linkStyle: {
+    textDecoration: 'none',
+    color: 'white'
   }
 };
 
@@ -38,11 +43,15 @@ function ButtonAppBar(props) {
           <Typography variant="title" color="inherit" className={classes.grow}>
             Marketplace
           </Typography>
+          
+          <Link to='/cart' className={classes.linkStyle}>
           <IconButton color="inherit">
             <Badge badgeContent={0} color="primary" classes={{ badge: classes.badge }}>
               <ShoppingCart>CART</ShoppingCart>
             </Badge>
           </IconButton>
+          </Link>
+          
         </Toolbar>
       </AppBar>
     </div>
