0a2a22ac37236f2b9d9f2e214e11b180e2557b26
Jin Park <jinpark1dev@gmail.com>
2018-09-25 13:05:54 -0700

    Added cart route.

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Market from './components/Market/Market';
 import ProductDetails from './components/ProductDetails/ProductDetails';
+import Cart from './components/Cart/Cart';
 
 export default (
     <Switch>
         <Route exact path='/' component={Market} />
         <Route path='/product/:id' component={ProductDetails} />
+        <Route path='/cart' component={Cart} />
     </Switch>
 )
